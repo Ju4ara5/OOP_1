5b9ff6a29088a14ab289ca23bbb869e1b47cb674
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2021-12-19 15:03:56 +0200

    7

--- encapsulation.py
+++ encapsulation.py
@@ -12,15 +12,29 @@
 # pt = Point(1, 2)
 # print(pt._x, pt._y)
 
+from accessify import private, protected
+
+
 class Point:
     def __init__(self, x=0, y=0):
-        self.__x = x
-        self.__y = y
+        self.__x = self.__y = 0
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
+
+    #  приватный метод:
+    #  @private  #  защита обращения к методу. при этом можно убрать __ из имени метода
+    @classmethod
+    def __check_value(cls, x):
+        return type(x) in (int, float)
 
     #  метод setter:
     def set_coord(self, x, y):
-        self.__x = x
-        self.__y = y
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError('Координаты должны быть числами')
 
     #  метод getter:
     def get_coord(self):
@@ -30,6 +44,7 @@ class Point:
 pt = Point(1, 2)
 pt.set_coord(10, 20)
 print(pt.get_coord())
-# print(pt.__x, pt.__y)  #  так вызвать неполучится
 
-# 7video  6 min
+# print(pt._Point__x)  # обращение к значению не рекомендуется использовать
+# print(dir(pt)) #  проверка возможных свойств класса pt
+# print(pt.__x, pt.__y)  #  так вызвать неполучится
