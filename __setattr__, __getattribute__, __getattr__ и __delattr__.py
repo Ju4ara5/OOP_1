4335e60dd831eb3cf913c4f8badbea6f4381c2d8
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2021-12-26 10:10:21 +0200

    1

--- __setattr__, __getattribute__, __getattr__ и __delattr__.py
+++ __setattr__, __getattribute__, __getattr__ и __delattr__.py
@@ -6,43 +6,39 @@ class Point:
         self.x = x
         self.y = y
 
-
     def set_coord(self, x, y):
         self.x = x
         self.y = y
 
-#  метод которы изменяет зачение атрибута MIN_COORD:
-#    @classmethod
-#    def change(cls, left):
-#        cls.MIN_COORD = left
-
+    #  метод которы изменяет зачение атрибута MIN_COORD:
+    #    @classmethod
+    #    def change(cls, left):
+    #        cls.MIN_COORD = left
 
     def __getattribute__(self, item):  # автоматически запускается при обращении к какому либо атрибуту класса
-        #print('__getattribute__: ', item)
+        # print('__getattribute__: ', item)
         if item == 'x':  # запрещаем доступ к атрибуту x.
             raise ValueError('Доступ к "x" запрещен')  # сгенерировать ошибку
         else:
             return object.__getattribute__(self, item)
 
-
-# автом вызывается когда идет присвоение какому либо атрибуту какого-то значания:
-# key - имя атрибута, value - значение которое ему присваивается:
-# можем запретить создавать какой либо атрибут в экземпляр класса:
+    # автом вызывается когда идет присвоение какому либо атрибуту какого-то значания:
+    # key - имя атрибута, value - значение которое ему присваивается:
+    # можем запретить создавать какой либо атрибут в экземпляр класса:
     def __setattr__(self, key, value):
-        #print('__setattr__: ', key, ':', value)
+        # print('__setattr__: ', key, ':', value)
         if key == 'z':
             raise AttributeError('Запрещенное имя атрибута')
         else:
             object.__setattr__(self, key, value)
 
-
-# вызывается автоматически когда идет обращение к несуществующему атрибуту экземпляра класса:
-#  можно использовать для избежания ошибки работы программы
+    # вызывается автоматически когда идет обращение к несуществующему атрибуту экземпляра класса:
+    #  можно использовать для избежания ошибки работы программы
     def __getattr__(self, item):
         print('__getattr__: ', 'атрибут', item, 'не существует.')
         return False
 
-# вызываетя когда удаляется атрибут экземпляра! класса:
+    # вызываетя когда удаляется атрибут экземпляра! класса:
     def __delattr__(self, item):
         print('__delattr__', item)
         object.__delattr__(self, item)
@@ -52,6 +48,4 @@ pt1 = Point(1, 2)
 pt2 = Point(10, 20)
 
 #  a = pt1.x  #  Доступ к "x" запрещен. т.к. это прописано в методе __getattribute__
-#pt1.z = 5  # AttributeError: Запрещенное имя атрибута. прописано в __setattr__
-
-
+# pt1.z = 5  # AttributeError: Запрещенное имя атрибута. прописано в __setattr__
